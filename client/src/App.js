625aa4f50a6e696503cfa48c5b1a51c0cccdfbfc
boughanmi-seifeddine <boughanmi1.seifeddine@gmail.com>
2023-03-15 16:48:28 +0100

    refactoring app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,143 +1,99 @@
-import React, {Component} from 'react';
+import {useState, useEffect} from "react";
+
 import axios from "axios";
 import './components/list/list.component'
 import ListComponent from "./components/list/list.component";
 import IncomeExpense from "./components/incomeexpense/incomeexpence.component";
 import AddTransaction from "./components/addTransaction/addTransaction.component";
 
-const dummyTransactions = [];
+function App() {
+    const [textInput, setTextInput] = useState("");
+    const [amountInput, setAmountInput] = useState(0);
+    const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
+    const [transactions, setTransactions] = useState([]);
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            amountInput: '',
-            textInput: '',
-            income: 0,
-            expense: 0,
-            transactions: []
-        }
-    }
+    useEffect(() => {
+        getTransactions().then((transactions) => {
+            setTransactions(transactions)
+            setIncome(transactions.reduce((cum, transaction) => {
+                return cum += transaction.amount > 0 ? transaction.amount : 0
+            }, 0))
+            setExpense(transactions.reduce((cum, transaction) => {
+                return cum += transaction.amount < 0 ? transaction.amount : 0
+            }, 0))
+        })
+    }, []);
 
-    generateId() {
-        return Date.now();
-    }
-
-    getTransactions = async () => {
+    const getTransactions = async () => {
         const response = await axios.get(
             `api/v1/transactions`
         );
         return response.data.data
     }
 
-    componentDidMount() {
-        this.getTransactions().then((transactions) => {
-            this.setState(() => {
-                return {transactions: transactions}
-            }, () => {
-                this.setState(() => {
-                    return {
-                        income: this.state.transactions.reduce((cum, transaction) => {
-                            return cum += transaction.amount > 0 ? transaction.amount : 0
-                        }, 0),
-                        expense: this.state.transactions.reduce((cum, transaction) => {
-                            return cum += transaction.amount < 0 ? transaction.amount : 0
-                        }, 0)
-                    }
-                })
-            })
+    const onTextInputChangeHandler = (e) => {
+        e.preventDefault()
+        setTextInput(e.target.value)
+    }
+
+    const onAmountInputChangeHandler = (e) => {
+        e.preventDefault()
+        setAmountInput(e.target.value * 1)
+    }
+
+    const onDeleteButtonClickHandler = (id) => {
+        axios.delete(`api/v1/transactions/${id}`).then((response) => {
+            const deletedTransaction = [...response.data.data].shift()
+            setTransactions(transactions.filter(element => element.id !== deletedTransaction.id))
+            setIncome(deletedTransaction.amount > 0 ? income - deletedTransaction.amount : income)
+            setExpense(deletedTransaction.amount < 0 ? expense - deletedTransaction.amount : expense)
         })
     }
 
-    onTextInputChangeHandler(e) {
+    const onFormSubmitHandler = (e) => {
         e.preventDefault()
-        this.setState(() => {
-            return {textInput: e.target.value}
-        })
-    }
-
-    onAmountInputChangeHandler(e) {
-        e.preventDefault()
-        this.setState(() => {
-            return {amountInput: e.target.value * 1}
-        })
-    }
-
-    onFormSubmitHandler(e) {
-        e.preventDefault()
-        if (this.state.textInput && this.state.amountInput) {
-            axios.post('api/v1/transactions', {text: this.state.textInput, amount: this.state.amountInput}).then((response) => {
-                this.setState(() => {
-                    return {
-                        transactions: [...this.state.transactions, {
-                            id: response.data.data[0].id,
-                            text: this.state.textInput,
-                            amount: this.state.amountInput
-                        }],
-                        income: this.state.amountInput > 0 ? this.state.income + this.state.amountInput : this.state.income,
-                        expense: this.state.amountInput < 0 ? this.state.expense - this.state.amountInput : this.state.expense,
-                        amountInput: '',
-                        textInput: ''
-                    }
-                })
+        if (textInput && amountInput) {
+            axios.post('api/v1/transactions', {text: textInput, amount: amountInput}).then((response) => {
+                setTransactions([...transactions, {
+                    id: response.data.data[0].id,
+                    text: textInput,
+                    amount: amountInput
+                }])
+                setTextInput('')
+                setAmountInput(0)
+                setIncome(amountInput > 0 ? income + amountInput : income)
+                setExpense(amountInput < 0 ? expense - amountInput : expense)
             })
         }
     }
 
-    onDeleteButtonClickHandler = (id) => {
-        axios.delete(`api/v1/transactions/${id}`).then((response) => {
-            const deletedTransaction = [...response.data.data].shift()
-            this.setState(() => {
-                return {
-                    transactions: this.state.transactions.filter(element => element.id !== deletedTransaction.id),
-                    income: deletedTransaction.amount > 0 ? this.state.income - deletedTransaction.amount : this.state.income,
-                    expense: deletedTransaction.amount < 0 ? this.state.expense - deletedTransaction.amount : this.state.expense,
-                }
-            })
-        })
-    }
+    return (
+        <div className="App">
+            <div className="App-container">
+                <h2>Expense Tracker</h2>
+                <div className="container">
+                    <h4>Your Balance</h4>
+                    <h1 id="balance">${((income + expense) * 1).toFixed(2)}</h1>
 
-    renderTransaction() {
-        return this.state.transactions.map(transaction => {
-            return <div key={transaction.id}>
-                <li className={transaction.amount >= 0 ? 'plus' : 'minus'}>
-                    {transaction.text} <span>{transaction.amount}</span>
-                    <button className="delete-btn">x</button>
-                </li>
-            </div>
-        })
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <div className="App-container">
-                    <h2>Expense Tracker</h2>
-
-                    <div className="container">
-                        <h4>Your Balance</h4>
-                        <h1 id="balance">${((this.state.income + this.state.expense) * 1).toFixed(2)}</h1>
-
-                        <div className="inc-exp-container">
-                            <IncomeExpense income={this.state.income} expense={this.state.expense}></IncomeExpense>
-                        </div>
-
-                        <h3>History</h3>
-                        <ListComponent transactions={this.state.transactions}
-                                       onDeleteButtonClick={this.onDeleteButtonClickHandler}></ListComponent>
-
-                        <h3>Add new transaction</h3>
-                        <AddTransaction onFormSubmit={this.onFormSubmitHandler.bind(this)}
-                                        onTextInputChange={this.onTextInputChangeHandler.bind(this)}
-                                        onAmountInputChange={this.onAmountInputChangeHandler.bind(this)}
-                                        textInput={this.state.textInput} amountInput={this.state.amountInput}>
-
-                        </AddTransaction>
+                    <div className="inc-exp-container">
+                        <IncomeExpense income={income} expense={expense}></IncomeExpense>
                     </div>
                 </div>
+                <h3>History</h3>
+                <ListComponent transactions={transactions}
+                               onDeleteButtonClick={onDeleteButtonClickHandler}></ListComponent>
+                <h3>Add new transaction</h3>
+                <AddTransaction onFormSubmit={onFormSubmitHandler.bind(this)}
+                                onTextInputChange={onTextInputChangeHandler.bind(this)}
+                                onAmountInputChange={onAmountInputChangeHandler.bind(this)}
+                                textInput={textInput} amountInput={amountInput}>
+
+                </AddTransaction>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default App;
+
