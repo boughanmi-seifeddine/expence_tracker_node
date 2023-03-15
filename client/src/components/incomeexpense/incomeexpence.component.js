9805d1fd2ba121247059644b18f5eabff4fc0fb5
boughanmi-seifeddine <boughanmi1.seifeddine@gmail.com>
2023-03-15 17:43:58 +0100

    fix fragment import

--- client/src/components/incomeexpense/incomeexpence.component.js
+++ client/src/components/incomeexpense/incomeexpence.component.js
@@ -1,6 +1,6 @@
-
+import  {Fragment} from "react";
 function IncomeExpenseComponent (props){
-        return <div >
+        return <Fragment>
             <div>
                 <h4>Income</h4>
                 <p id="money-plus" className="money plus">+${props.income}</p>
@@ -9,7 +9,7 @@ function IncomeExpenseComponent (props){
                 <h4>Expense</h4>
                 <p id="money-minus" className="money minus">-${props.expense}</p>
             </div>
-        </div>
+        </Fragment>
 
 }
 export default IncomeExpenseComponent
